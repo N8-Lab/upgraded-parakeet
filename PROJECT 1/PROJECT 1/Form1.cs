2a051c3f045a6205b5f3ba116415ff84c66d64cd
N8-Lab <80381787+N8-Lab@users.noreply.github.com>
2021-05-24 21:21:43 +1200

    Add files via upload

---
+++ PROJECT 1/PROJECT 1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PROJECT_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Red;
+            label1.AutoSize = true;
+            button1.Text = "HI";
+            button1.ForeColor = Color.Magenta;
+            label1.BorderStyle = BorderStyle.Fixed3D;
+            
+        }
+    }
+}
